9b50c0e0315104bc140c5ad12f296f9482f3a3b3
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2017-07-07 14:57:55 +0300

    Prepared main activity

--- app/src/main/java/com/mobiledevpro/demo/mediagallery/MainActivity.java
+++ app/src/main/java/com/mobiledevpro/demo/mediagallery/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mobiledevpro.demo.mediagallery;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        TextView tvContactUs = (TextView) findViewById(R.id.tv_contact_us);
+        if (tvContactUs != null) {
+            tvContactUs.setMovementMethod(LinkMovementMethod.getInstance());
+        }
     }
 }
